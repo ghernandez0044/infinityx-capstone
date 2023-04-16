1ef60f5deabfc8344ce8e03a2adaa9bea4d27a4d
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-04-16 13:50:38 -0700

    generated a new revision to fix the rest

--- app/models/schedule.py
+++ app/models/schedule.py
@@ -3,6 +3,10 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 class Schedule(db.Model):
     __tablename__ = 'schedules'
 
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
     launch_spaceport_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('spaceports.id')))
     landing_spaceport_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('spaceports.id')))
     departure_time = db.Column(db.String(100), nullable=False)
