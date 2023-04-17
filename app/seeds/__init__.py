667740f56042d411b91cc2cf238c3b0d6d772bea
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-04-17 16:42:09 -0700

    progress check

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -3,6 +3,7 @@ from .users import seed_users, undo_users
 from .planets import seed_planets, undo_planets
 from .planet_comments import seed_planet_comments, undo_planet_comments
 from .wallets import seed_wallets, undo_wallets
+from .spaceports import seed_spaceports, undo_spaceports
 
 from app.models.db import db, environment, SCHEMA
 
@@ -19,6 +20,7 @@ def seed():
         # command, which will  truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_spaceports()
         undo_wallets()
         undo_planet_comments()
         undo_planets()
@@ -27,12 +29,14 @@ def seed():
     seed_planets()
     seed_planet_comments()
     seed_wallets()
+    seed_spaceports()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_spaceports()
     undo_wallets()
     undo_planet_comments()
     undo_planets()
