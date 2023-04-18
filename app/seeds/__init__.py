e944f0194154461f161b160d99cbf13b3052b1e3
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-04-17 21:15:58 -0700

    added seed file for travel classes

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -7,6 +7,7 @@ from .spaceports import seed_spaceports, undo_spaceports
 from .spacecrafts import seed_spacecraft, undo_spacecraft
 from .tiers import seed_tiers, undo_tiers
 from .frequent_flyers import seed_frequent_flyers, undo_frequent_flyers
+from .travel_classes import seed_travel_class, undo_travel_class
 
 from app.models.db import db, environment, SCHEMA
 
@@ -23,6 +24,7 @@ def seed():
         # command, which will  truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_travel_class()
         undo_frequent_flyers()
         undo_tiers()
         undo_spacecraft()
@@ -39,12 +41,14 @@ def seed():
     seed_spacecraft()
     seed_tiers()
     seed_frequent_flyers()
+    seed_travel_class()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_travel_class()
     undo_frequent_flyers()
     undo_tiers()
     undo_spacecraft()
---
+++ app/seeds/travel_classes.py
@@ -0,0 +1,30 @@
+from app.models import db, User, Planet, Wallet, Spaceport, FrequentFlyer, TravelClass, environment, SCHEMA
+from sqlalchemy.sql import text
+
+def seed_travel_class():
+    class1 = TravelClass(
+        name = 'Base Class',
+        description = 'With Base class, you will be traveling in a standard seat'
+    )
+    class2 = TravelClass(
+        name = 'Cruise Class',
+        description = 'With Cruise Class, you will be traveling in a standard-plus seat and will recieve 1 star for every 100 dollars spent'
+    )
+    class3 = TravelClass(
+        name = 'Launch Class',
+        description = 'With Launch Class, you will be traveling in a premium seat and will recieve 1 star for every 50 dollars spent'
+    )
+
+    db.session.add(class1)
+    db.session.add(class2)
+    db.session.add(class3)
+    db.session.commit()
+
+
+def undo_travel_class():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.travel_classes RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM travel_classes"))
+
+    db.session.commit() 
