99ead3fc693c9e75d90da7bfa62b4c58e3d238b6
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-04-18 22:13:27 -0700

    created spacecraft form and create a spacecraft route

--- app/api/spacecraft_routes.py
+++ app/api/spacecraft_routes.py
@@ -4,6 +4,7 @@ from flask_login import current_user, login_required
 from sqlalchemy.orm import joinedload
 
 from ..models import db, Spacecraft
+from ..forms import SpacecraftForm
 
 spacecraft_routes = Blueprint('spacecrafts', __name__)
 
@@ -20,4 +21,28 @@ def get_one_spacecraft():
     return one_spacecraft.to_dict()
 
 # Create a spacecraft route
+@spacecraft_routes.route('/spacecraft', methods=['POST'])
+def create_spacecraft():
+    user = current_user.to_dict()
+    form = SpacecraftForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
 
+    if user.admin:
+        if form.validate_on_submit():
+            new_spacecraft = Spacecraft(
+                user_id = user["id"],
+                model = form.data["model"],
+                load_capacity_kg = form.data["load_capacity_kg"],
+                description = form.data["description"],
+                height_m = form.data["height_m"],
+                diameter_m = form.data["diameter_m"],
+                mass_kg = form.data["mass_kg"],
+                capsule_volume_m = form.data["capsule_volume_m"],
+                trunk_volume_m = form.data["trunk_volume_m"]
+            )
+            db.session.add(new_spacecraft)
+            db.session.commit()
+            return {"spacecraft": new_spacecraft, "user": new_spacecraft.user.to_dict()}
+        if form.errors:
+            return {"message": "form errors", "errors": f"{form.errors}"}
+        return {"message": "bad data or user is not an admin"}
