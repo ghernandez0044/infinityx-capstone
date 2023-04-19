7c405fc0639ad2efb5eaf9c9b4bf4b942665600e
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-04-18 22:58:55 -0700

    completed create a spaceport route

--- app/api/spaceport_routes.py
+++ app/api/spaceport_routes.py
@@ -19,3 +19,27 @@ def get_all_spaceport():
 def get_one_spaceport(id):
     one_spaceport = Spaceport.query.get(id)
     return one_spaceport.to_dict()
+
+# Create a spaceport route
+@spaceport_routes.route('/', methods=["POST"])
+def create_spaceport():
+    user = current_user.to_dict()
+    form = SpaceportForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+
+    if user.admin:
+        if form.validate_on_submit():
+            new_spaceport = Spaceport(
+                name = form.data["name"],
+                description = form.data["description"],
+                city = form.data["city"],
+                state = form.data["state"],
+                lat = form.data["lat"],
+                lng = form.data["lng"]
+            )
+            db.session.add(new_spaceport)
+            db.session.commit()
+            return {"spaceport": new_spaceport.to_dict()}
+        if form.errors:
+            return {"message": "form errors", "errors": f"{form.errors}"}
+    return {"message": "user is not an admin"}
