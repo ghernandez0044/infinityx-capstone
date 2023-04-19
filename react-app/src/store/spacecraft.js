937ec898b38bf5aae90be172bbed54e6a08020b9
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-04-19 08:26:12 -0700

    working on store for spacecraft state slice

---
+++ react-app/src/store/spacecraft.js
@@ -0,0 +1,44 @@
+// Type Variables
+const LOAD_SPACECRAFTS = "spacecrafts/loadSpacecrafts"
+const LOAD_SPACECRAFT = "spacecrafts/loadSpacecraft"
+const CREATE_SPACECRAFT = "spacecrafts/createSpacecraft"
+const UPDATE_SPACECRAFT = "spacecrafts/updateSpacecraft"
+const DELETE_SPACECRAFT = "spacecrafts/deleteSpacecraft"
+
+// Action Creators
+export const actionLoadSpacecrafts = (spacecrafts) => {
+    return {
+        type: LOAD_SPACECRAFTS,
+        spacecrafts
+    }
+}
+
+export const actionLoadSpacecraft = (spacecraft) => {
+    return {
+        type: LOAD_SPACECRAFT,
+        spacecraft
+    }
+}
+
+export const actionCreateSpacecraft = (spacecraft) => {
+    return {
+        type: CREATE_SPACECRAFT,
+        spacecraft
+    }
+}
+
+export const actionUpdateSpacecraft = (spacecraft) => {
+    return {
+        type: UPDATE_SPACECRAFT,
+        spacecraft
+    }
+}
+
+export const actionDeleteSpacecraft = (spacecraft) => {
+    return {
+        type: DELETE_SPACECRAFT,
+        spacecraft
+    }
+}
+
+// Thunks
