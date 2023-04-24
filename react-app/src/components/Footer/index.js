baa63779b2224bdbbdd59cc2cbc71ab6f45c3736
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-04-24 08:27:14 -0700

    added gallery for spaceports

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -18,6 +18,19 @@ function Footer(){
             <NavLink exact to='https://www.linkedin.com/in/guillermo-hernandez-32a307180/'>
                 <i className="fa-brands fa-linkedin fa-xl" />
             </NavLink>
+            {/* <div>
+                Image by <a href="https://pixabay.com/users/wikiimages-1897/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=67724">WikiImages</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=67724">Pixabay</a>
+
+            </div>
+            <div>
+            Image by <a href="https://pixabay.com/users/wikiimages-1897/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=67643">WikiImages</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=67643">Pixabay</a>
+            </div> */}
+            {/* <div>
+            Image by <a href="https://pixabay.com/users/qimono-1962238/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1365995">Arek Socha</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1365995">Pixabay</a>
+            </div> */}
+            {/* <div>
+            Image by <a href="https://pixabay.com/users/spacex-imagery-885857/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=693236">SpaceX-Imagery</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=693236">Pixabay</a>
+            </div> */}
         </footer>
     )
 }
--- react-app/src/components/GalleryCard/index.js
+++ react-app/src/components/GalleryCard/index.js
@@ -5,21 +5,48 @@ import { useDispatch } from "react-redux";
 import './GalleryCard.css'
 
 function GalleryCard({ smallTag, bigTag, buttonText, destination, payload }){
+
+    let choice = ''
+
+    if(destination === 'spacecrafts'){
+        // history.push(`/spacecrafts/${payload.id}`)
+        choice = 'spacecraft-background'
+    }
+
+    if(destination === 'spaceports'){
+        console.log()
+        // history.push(`/spaceports/${payload.id}`)
+        choice = 'spaceport-background'
+    }
+
+    if(destination === 'planets'){
+        // history.push(`/planets/${payload.id}`)
+        choice = 'planet-background'
+    }
+
+
     // Create history method
     const history = useHistory()
 
     // Function to handle button click
     const onClick = () => {
-        if(destination === 'spacecrafts') history.push(`/spacecrafts/${payload.id}`)
+        if(destination === 'spacecrafts'){
+            history.push(`/spacecrafts/${payload.id}`)
+        }
 
-        if(destination === 'spaceports') history.push(`/spaceports/${payload.id}`)
+        if(destination === 'spaceports'){
+            console.log()
+            history.push(`/spaceports/${payload.id}`)
+        }
 
-        if(destination === 'planets') history.push(`/planets/${payload.id}`)
+        if(destination === 'planets'){
+            history.push(`/planets/${payload.id}`)
+        }
     }
 
 
     return (
-        <div className="section-a-container sizing">
+        <div className={`section-a-container sizing ${choice}`}>
             <div className="section-content">
                 <h4>{smallTag}</h4>
                 <h2>{bigTag}</h2>
