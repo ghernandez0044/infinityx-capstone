3d4483f3e624fc3c23769edfccbdbc1ece4a8151
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-05-05 09:52:57 -0700

    added error validations on spacecraft form

--- react-app/src/components/SpacecraftForm/index.js
+++ react-app/src/components/SpacecraftForm/index.js
@@ -50,6 +50,36 @@ function SpacecraftForm({ edit, payload }){
         setTrunkVolume('')
     }
 
+     // Check for validation errors on form inputs
+     useEffect(() => {
+        const validationErrors = {}
+
+        if(model.length > 255) validationErrors.modelErr = 'Model must be less than 255 characters'
+        if(model.length <= 0) validationErrors.modelErr = 'Model is required'
+        if(year < 2015 || year > 2200) validationErrors.yearErr = 'Year must be between 2015 and 2200'
+        if(year.toString().length === 0) validationErrors.yearErr = 'Year is required'
+        if(description.length === 0) validationErrors.descriptionErr = 'Description is required'
+        if(loadCapacity > 250000 || loadCapacity < 2000) validationErrors.loadCapacityErr = "Load Capacity must be between 2,000 and 250,000 kg"
+        if(loadCapacity.toString().length === 0) validationErrors.loadCapacityErr = 'Load Capacity is required'
+        if(height > 200 || height < 1) validationErrors.heightErr = 'Height must be between 1 and 200 m'
+        if(height.toString().length === 0) validationErrors.heightErr = 'Height is required'
+        if(diameter > 100 || diameter < 1) validationErrors.diameterErr = 'Diameter must be between 1 and 100 m'
+        if(diameter.toString().length === 0) validationErrors.diameterErr = 'Diameter is required'
+        if(mass > 10000000 || mass < 10) validationErrors.massErr = "Mass must be between 10 and 10,000,000 kg"
+        if(mass.toString().length === 0) validationErrors.massErr = 'Mass is required'
+        if(capsuleVolume > 100 || capsuleVolume < 1) validationErrors.capsuleVolumeErr = 'Capsule Volume must be between 1 and 100 m'
+        if(capsuleVolume.toString().length === 0) validationErrors.capsuleVolumeErr = 'Capsule Volume is required'
+        if(trunkVolume > 100 || trunkVolume < 10) validationErrors.trunkVolumeErr = 'Trunk Volume must be between 10 and 100 m'
+        if(trunkVolume.toString().length === 0) validationErrors.trunkVolumeErr = 'Trunk Volume is required'
+
+        setErrors(validationErrors)
+
+        console.log('errors: ', errors)
+        console.log('isSubmitted: ', isSubmitted)
+
+
+    }, [ model, year, loadCapacity, description, height, diameter, mass, capsuleVolume, trunkVolume ])
+
     // Subscribe to current user slice of state
     const user = useSelector(state => state.session.user)
 
@@ -57,96 +87,48 @@ function SpacecraftForm({ edit, payload }){
 
     if(!user.admin) return <h1>User Is Not An Admin</h1>
 
-
     // Create onSubmit function
     const onSubmit = async (e) => {
         e.preventDefault()
         setIsSubmitted(true)
-        const backendErrorObj = {}
 
         if(Object.keys(errors).length === 0){
             const payload = {
-                        model,
-                        "year": Number(year),
-                        "load_capacity_kg": Number(loadCapacity),
-                        description,
-                        "height_m": Number(height),
-                        "diameter_m": Number(diameter),
-                        "mass_kg": Number(mass),
-                        "capsule_volume_m": Number(capsuleVolume),
-                        "trunk_volume_m": Number(trunkVolume)
-                    }
+                model,
+                "year": Number(year),
+                "load_capacity_kg": Number(loadCapacity),
+                description,
+                "height_m": Number(height),
+                "diameter_m": Number(diameter),
+                "mass_kg": Number(mass),
+                "capsule_volume_m": Number(capsuleVolume),
+                "trunk_volume_m": Number(trunkVolume)
+            }
     
             // If the edit flag is true, run the edit dispatch instead of the create dispatch
             if(edit){
-                const editedSpacecraft = await dispatch(updateSpacecraft(payload, id)).catch(async (res) => {
-                    const data = await res.json()
-                    if(data && data.errors) backendErrorObj.editErrors = data.errors
-                })
-                setIsSubmitted(false)
-                reset()
-                setBackendErrors(backendErrorObj)
-                history.push(`/spacecrafts/${res.spacecraft.id}`)
-                return
-            } else {
-                const createdSpacecraft = await dispatch(createSpacecraft(payload)).catch(async (res) => {
-                    const data = await res.json()
-                    if(data && data.errors) backendErrorObj.createErrors = data.errors
-                    setIsSubmitted(false)
-                    reset()
-                    setBackendErrors(backendErrorObj)
+                dispatch(updateSpacecraft(payload, id)).then(res => {
                     history.push(`/spacecrafts/${res.spacecraft.id}`)
-                    return
-                })
+                    setIsSubmitted(false)
+                }).catch(res => {
+                    const data = res
+                    if(data && data.errors) setBackendErrors(data.errors)
+                 })
+            } else {
+                dispatch(createSpacecraft(payload)).then(res => { 
+                    history.push(`/spacecrafts/${res.spacecraft.id}`)
+                    setIsSubmitted(false)
+                 }).catch(res => {
+                    const data = res
+                    if(data && data.errors){
+                        console.log('inside create spacecraft')
+                        setBackendErrors(data.errors)
+                        console.log('create a spacecraft backend errors: ', backendErrors)
+                    } 
+                 })
             }
         }
-
     }
-
-    // // Create onSubmit function
-    // const onSubmit = async (e) => {
-    //     e.preventDefault()
-
-    //     const payload = {
-    //         model,
-    //         "year": Number(year),
-    //         "load_capacity_kg": Number(loadCapacity),
-    //         description,
-    //         "height_m": Number(height),
-    //         "diameter_m": Number(diameter),
-    //         "mass_kg": Number(mass),
-    //         "capsule_volume_m": Number(capsuleVolume),
-    //         "trunk_volume_m": Number(trunkVolume)
-    //     }
-
-    //     // If the edit flag is true, run the edit dispatch instead of the create dispatch
-    //     if(edit){
-    //         dispatch(updateSpacecraft(payload, id)).then(res => {
-    //             history.push(`/spacecrafts/${res.spacecraft.id}`)
-    //         }).catch(res => {
-    //             const data = res
-    //             if(data && data.errors) setBackendErrors(data.errors)
-    //          })
-    //     } else {
-    //         dispatch(createSpacecraft(payload)).then(res => { 
-    //             history.push(`/spacecrafts/${res.spacecraft.id}`)
-    //          }).catch(res => {
-    //             const data = res
-    //             if(data && data.errors){
-    //                 console.log('inside create spacecraft')
-    //                 setBackendErrors(data.errors)
-    //                 console.log('create a spacecraft backend errors: ', backendErrors)
-    //             } 
-    //          })
-
-
-    //         // const data = await dispatch(createSpacecraft(payload))
-    //         // if(data) setBackendErrors(data)
-    //     }
-
-    // }
-
-    console.log('create a spacecraft backend errors: ', backendErrors)
 
 
     return user.admin && (
@@ -159,34 +141,43 @@ function SpacecraftForm({ edit, payload }){
 					))}
 				</ul> */}
                 <label className='label-font'>Model </label>
+                {isSubmitted && errors.modelErr && ( <div className='label-font spacecraft-errors'>{errors.modelErr}</div> )}
                 <input id='model' type='text' value={model} onChange={(e) => setModel(e.target.value)} required placeholder="Required">
                 </input>
                 <label className='label-font'>Year </label>
+                {isSubmitted && errors.yearErr && ( <div className='label-font spacecraft-errors'>{errors.yearErr}</div> )}
                 <input id='year' type='number' value={year} onChange={(e) => setYear(e.target.value)} required placeholder="Required">
                 </input>
                 <label className='label-font'>Load Capacity In Kilograms </label>
+                {isSubmitted && errors.loadCapacityErr && ( <div className='label-font spacecraft-errors'>{errors.loadCapacityErr}</div> )}
                 <input id='load_capacity_kg' type='number' value={loadCapacity} onChange={(e) => setLoadCapacity(e.target.value)} required placeholder="Required">
                 </input>
                 <label className='label-font'>Description </label>
+                {isSubmitted && errors.descriptionErr && ( <div className='label-font spacecraft-errors'>{errors.descriptionErr}</div> )}
                 <textarea id='description' value={description} onChange={(e) => setDescription(e.target.value)} required placeholder="Required">
                 </textarea>
                 <label className='label-font'>Height In Meters </label>
+                {isSubmitted && errors.heightErr && ( <div className='label-font spacecraft-errors'>{errors.heightErr}</div> )}
                 <input id='height_m' type='number' value={height} onChange={(e) => setHeight(e.target.value)} required placeholder="Required">
                 </input>
                 <label className='label-font'>Diameter In Meters </label>
+                {isSubmitted && errors.diameterErr && ( <div className='label-font spacecraft-errors'>{errors.diameterErr}</div> )}
                 <input id='diameter_m' type='number' value={diameter} onChange={(e) => setDiameter(e.target.value)} required placeholder="Required">
                 </input>
                 <label className='label-font'>Mass In Kilograms </label>
+                {isSubmitted && errors.massErr && ( <div className='label-font spacecraft-errors'>{errors.massErr}</div> )}
                 <input id='mass_kg' type='number' value={mass} onChange={(e) => setMass(e.target.value)} required placeholder="Required">
                 </input>
                 <label className='label-font'>Capsule Volume In Meters Squared </label>
+                {isSubmitted && errors.capsuleVolumeErr && ( <div className='label-font spacecraft-errors'>{errors.capsuleVolumeErr}</div> )}
                 <input id='capsule_volume_kg' type='number' value={capsuleVolume} onChange={(e) => setCapsuleVolume(e.target.value)} required placeholder="Required">
                 </input>
                 <label className='label-font'>Trunk Volume In Meters Squared </label>
+                {isSubmitted && errors.trunkVolumeErr && ( <div className='label-font spacecraft-errors'>{errors.trunkVolumeErr}</div> )}
                 <input id='trunk_volume_m' type='number' value={trunkVolume} onChange={(e) => setTrunkVolume(e.target.value)} required placeholder="Required">
                 </input>
                 {/* <button type='submit'>{!edit ? 'Create Spacecraft' : 'Edit Spacecraft'}</button> */}
-                <div type='submit' onClick={onSubmit} className="button animate">
+                <div onClick={onSubmit} className="button animate">
                     <div className="hover-effect"></div>
                     <span className="signup-button-font">{!edit ? 'Create Spacecraft' : 'Edit Spacecraft'}</span>
                 </div>
