21f794853c74318bdce74d78d988cbd1af58ea68
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-05-07 01:28:46 -0700

    created create a wallet route

--- app/api/wallet_routes.py
+++ app/api/wallet_routes.py
@@ -1,6 +1,7 @@
 # Necessary imports
 from flask import Blueprint, request
 from flask_login import current_user, login_required
+from secrets import token_hex
 
 from ..models import db, Wallet
 from ..forms import WalletForm
@@ -19,3 +20,15 @@ def get_one_wallet(id):
     one_wallet = Wallet.query.get(id)
     return one_wallet.to_dict()
 
+# Create a wallet route 
+@wallet_routes.route('/', methods=["POST"])
+def create_wallet():
+    user = current_user
+    new_wallet = Wallet(
+        user_id = user.id,
+        address = token_hex(16),
+        funds = 0
+    )
+    db.session.add(new_wallet)
+    db.session.commit()
+    return {"wallet": new_wallet.to_dict()}
