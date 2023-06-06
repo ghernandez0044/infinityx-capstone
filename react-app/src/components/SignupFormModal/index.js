c1c7e90d4ef826ec5b697c7b373fdfb8e09b41b7
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-06-05 23:51:12 -0700

    touched up sign up modal

--- react-app/src/components/SignupFormModal/index.js
+++ react-app/src/components/SignupFormModal/index.js
@@ -64,118 +64,124 @@ function SignupFormModal({ edit, payload }) {
 
 	return (
 		<div className="signup-modal-container">
-			<h1 className="header-font" style={{ textAlign: 'center' }}>{!edit ? 'Create User' : 'Edit User'}</h1>
+			<h1 className="header-font" style={{ textAlign: 'center', fontSize: '44px' }}>{!edit ? 'Create User' : 'Edit User'}</h1>
 			<form className="signup-form-container" onSubmit={handleSubmit}>
 				<ul>
 					{errors.map((error, idx) => (
 						<li key={idx}>{error}</li>
 					))}
 				</ul>
-				<label className='label-font'>
-					Email
-				</label>
-				<input
-						id='email'
-						type="email"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-						required
-				/>
-				<label className='label-font'>
-					Username
-				</label>
-				<input
-						id='username'
-						type="text"
-						value={username}
-						onChange={(e) => setUsername(e.target.value)}
-						required
-				/>
-				<label className='label-font'>
-					First Name
-				</label>
-				<input
-						id='first_name'
-						type="text"
-						value={firstName}
-						onChange={(e) => setFirstName(e.target.value)}
-						required
-				/>
-				<label className='label-font'>
-					Last Name
-				</label>
-				<input
-						id='last_name'
-						type="text"
-						value={lastName}
-						onChange={(e) => setLastName(e.target.value)}
-						required
-				/>
-				<label className='label-font'>
-					Phone
-				</label>
-				<input
-						id='phone'
-						type="text"
-						value={phone}
-						onChange={(e) => setPhone(e.target.value)}
-						required
-				/>
-				<label className='label-font'>
-					Profile Picture
-				</label>
-				<input
-						id='profile_pic'
-						type="text"
-						value={profilePic}
-						onChange={(e) => setProfilePic(e.target.value)}
-						required
-				/>
-				<label className='label-font'>Choose A Passport </label>
-              <select
-                id="passport"
-                onChange={(e) => {
-                  setPassport(e.target.value);
-                }}
-                value={passport}
-                name="passport"
-                placeholder="Choose A Passport"
-              >
-                {/* <option value=""></option> */}
-                {passports.map((c) => (
-                  <option value={c} key={c}>
-                    {c}
-                  </option>
-                  // add a key prop here
-                ))}
-              </select>
-			  {!edit ? (
-				<>
+				<div className="first-section-container">
 					<label className='label-font'>
-					Password
+						Email
 					</label>
 					<input
-						id='password'
-						type="password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						required
+							id='email'
+							type="email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							required
 					/>
 					<label className='label-font'>
-					Confirm Password
+						Username
 					</label>
 					<input
-						id='password'
-						type="password"
-						value={confirmPassword}
-						onChange={(e) => setConfirmPassword(e.target.value)}
-						required
+							id='username'
+							type="text"
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+							required
 					/>
-				</>
-			  ) : ( 
-				<>
-				</>
-			   )}
+					<label className='label-font'>
+						First Name
+					</label>
+					<input
+							id='first_name'
+							type="text"
+							value={firstName}
+							onChange={(e) => setFirstName(e.target.value)}
+							required
+					/>
+					<label className='label-font'>
+						Last Name
+					</label>
+					<input
+							id='last_name'
+							type="text"
+							value={lastName}
+							onChange={(e) => setLastName(e.target.value)}
+							required
+					/>
+				</div>
+				<div className="second-section-container">
+					<label className='label-font'>
+						Phone
+					</label>
+					<input
+							id='phone'
+							type="text"
+							value={phone}
+							onChange={(e) => setPhone(e.target.value)}
+							required
+					/>
+					<label className='label-font'>
+						Profile Picture
+					</label>
+					<input
+							id='profile_pic'
+							type="text"
+							value={profilePic}
+							onChange={(e) => setProfilePic(e.target.value)}
+							required
+					/>
+					<label className='label-font'>Choose A Passport </label>
+				<select
+					id="passport"
+					onChange={(e) => {
+					setPassport(e.target.value);
+					}}
+					value={passport}
+					name="passport"
+					placeholder="Choose A Passport"
+				>
+					{/* <option value=""></option> */}
+					{passports.map((c) => (
+					<option value={c} key={c}>
+						{c}
+					</option>
+					// add a key prop here
+					))}
+				</select>
+				</div>
+				<div className="third-section-container">
+				{!edit ? (
+					<>
+						<label className='label-font'>
+						Password
+						</label>
+						<input
+							id='password'
+							type="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							required
+						/>
+						<label className='label-font'>
+						Confirm Password
+						</label>
+						<input
+							id='password'
+							type="password"
+							value={confirmPassword}
+							onChange={(e) => setConfirmPassword(e.target.value)}
+							required
+						/>
+					</>
+				) : ( 
+					<>
+					</>
+				)}
+				</div>
 				 {/* <label className='label-font'>
 				 	Password
 				 </label>
@@ -199,11 +205,11 @@ function SignupFormModal({ edit, payload }) {
 				{/* <label>Admin</label>
 				<input id='admin' type='radio' checked={checked} value={admin} onClick={() => setChecked(!checked)} onChange={(e) => setAdmin(true)} /> */}
 				{/* <button type="submit">Sign Up</button> */}
-				<div onClick={handleSubmit} className="button animate">
-          			<div className="hover-effect"></div>
-          			<span className="signup-button-font">{!edit ? 'Create User' : 'Edit User'}</span>
-       			 </div>
 			</form>
+			<div style={{ margin: '25px auto', width: '500px' }} onClick={handleSubmit} className="button animate">
+          		<div className="hover-effect"></div>
+          		<span className="signup-button-font">{!edit ? 'Create User' : 'Edit User'}</span>
+       		</div>
 		</div>
 	);
 }
