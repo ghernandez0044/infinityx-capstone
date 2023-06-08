9a2d2812bc01240559caaa0f4613567b0802a034
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-06-08 01:06:42 -0700

    created search flight backend endpoint

--- app/api/flight_routes.py
+++ app/api/flight_routes.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import joinedload
 from .auth_routes import validation_errors_to_error_messages
 
 from ..models import db, Flight
+from ..forms import SearchFlightForm
 
 flight_routes = Blueprint('flights', __name__)
 
@@ -19,3 +20,17 @@ def get_all_flights():
 def get_one_flight(id):
     flight = Flight.query.get(id)
     return flight.to_dict()
+
+# Search for a flight
+@flight_routes.route('/flights/search')
+def search_flight():
+    user = current_user
+    form = SearchFlightForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+
+    desired_orbit = form.data["orbit"]
+    earliest_date = form.data["date"]
+
+    available_flights = Flight.query.filter(Flight.orbit.ilike(f"%{desired_orbit}%") ).filter(Flight.departure_time.like(f"%{earliest_date}%"))
+
+    return [flight.to_dict() for flight in available_flights]
