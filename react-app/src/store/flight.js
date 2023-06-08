6ec8af80f86c3a0ba47f4834ce403f988b017bde
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-06-08 01:09:30 -0700

    progress check, started work on flights slice of state

---
+++ react-app/src/store/flight.js
@@ -0,0 +1,6 @@
+// Type Variables
+const LOAD_FLIGHTS = "flights/loadFlights"
+const LOAD_FLIGHT = "flights/loadFlight"
+const CREATE_FLIGHT = "flights/createFlight"
+const DELETE_FLIGHT = "flights/deleteFlight"
+
