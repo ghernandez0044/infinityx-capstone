22ddf830b1cc3c66c1af5d1d2bfe6ac3ea92c7aa
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-06-08 01:13:24 -0700

    added action creators to flights slice of state

--- react-app/src/store/flight.js
+++ react-app/src/store/flight.js
@@ -1,6 +1,28 @@
 // Type Variables
 const LOAD_FLIGHTS = "flights/loadFlights"
 const LOAD_FLIGHT = "flights/loadFlight"
+const SEARCH_FLIGHTS = "flights/searchFlights"
 const CREATE_FLIGHT = "flights/createFlight"
 const DELETE_FLIGHT = "flights/deleteFlight"
 
+// Action Creators
+export const actionLoadFlights = (flights) => {
+    return {
+        type: LOAD_FLIGHTS,
+        flights
+    }
+}
+
+export const actionLoadFlight = (flight) => {
+    return {
+        type: LOAD_FLIGHT,
+        flight
+    }
+}
+
+export const actionSearchFlights = (flights) => {
+    return {
+        type: SEARCH_FLIGHTS,
+        flights
+    }
+}
