ba809e5570713194165daa8b462a6fea66846ca3
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-06-09 05:40:54 -0700

    added action creators for bookings

--- react-app/src/store/bookings.js
+++ react-app/src/store/bookings.js
@@ -3,3 +3,24 @@ const LOAD_BOOKINGS = "bookings/loadBookings"
 const LOAD_BOOKING = "bookings/loadBooking"
 const CREATE_BOOKING = "bookings/createBooking"
 
+// Action Creators
+export const actionLoadBookings = (bookings) => {
+    return {
+        type: LOAD_BOOKINGS,
+        bookings
+    }
+}
+
+export const actionLoadBooking = (booking) => {
+    return {
+        type: LOAD_BOOKING,
+        booking
+    }
+}
+
+export const actionCreateBooking = (booking) => {
+    return {
+        type: CREATE_BOOKING,
+        booking
+    }
+}
