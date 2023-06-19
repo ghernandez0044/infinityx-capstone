0bd4aee008077fa5b455c1c92b5e07136c2f1d50
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-06-19 02:02:59 -0700

    progress check, working on create a transaction

--- react-app/src/store/transactions.js
+++ react-app/src/store/transactions.js
@@ -1,5 +1,6 @@
 // Type Variables
 const LOAD_TRANSACTIONS = "transactions/loadTransactions"
+const LOAD_USER_TRANSACTIONS = "transactions/loadUserTransactions"
 const LOAD_TRANSACTION = "transactions/loadTransaction"
 const CREATE_TRANSACTION = "transactions/createTransaction"
 
@@ -7,6 +8,13 @@ const CREATE_TRANSACTION = "transactions/createTransaction"
 export const actionLoadTransactions = (transactions) => {
     return {
         type: LOAD_TRANSACTIONS,
+        transactions
+    }
+}
+
+export const actionLoadUserTransactions = (transactions) => {
+    return {
+        type: LOAD_USER_TRANSACTIONS,
         transactions
     }
 }
@@ -20,7 +28,48 @@ export const actionLoadTransaction = (transaction) => {
 
 export const actionCreateTransaction = (transaction) => {
     return {
-        type: LOAD_TRANSACTION,
+        type: CREATE_TRANSACTION,
         transaction
     }
+}
+
+// Thunks
+export const getAllTransactions = () => async (dispatch) => {
+    const res = await fetch('/api/transactions')
+    if(res.ok){
+        const transactions = res.json()
+        dispatch(actionLoadTransactions(transactions))
+        return transactions
+    }
+    return res
+}
+
+export const getAllUserTransactions = (userId) => async (dispatch) => {
+    const res = await fetch(`/api/users/${userId}/transactions`)
+    if(res.ok){
+        const transactions = res.json()
+        dispatch(actionLoadUserTransactions(transactions))
+        return transactions
+    }
+    return res
+}
+
+export const getOneTransaction = (transactionId) => async (dispatch) => {
+    const res = await fetch(`/api/transactions/${transactionId}`)
+    if(res.ok){
+        const transaction = res.json()
+        dispatch(actionLoadTransaction(transaction))
+        return transaction
+    }
+    return res
+}
+
+export const createOneTransaction = (transaction) => async (dispatch) => {
+    const res = await fetch(`/api/users/${transaction.user_id}/transactions`)
+    if(res.ok){
+        const transaction = res.json()
+        dispatch(actionCreateTransaction(transaction))
+        return transaction
+    }
+    return res
 }
