214becc73fe0c3deb1bce35bc2dd43c4d9453f15
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-06-19 01:52:38 -0700

    progress check, starting slice of state for transactions

---
+++ react-app/src/store/transactions.js
@@ -0,0 +1,5 @@
+// Type Variables
+const LOAD_TRANSACTIONS = "transactions/loadTransactions"
+const LOAD_TRANSACTION = "transactions/loadTransaction"
+const CREATE_TRANSACTION = "transactions/createTransaction"
+
