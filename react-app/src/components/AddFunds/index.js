61ae635994388933382d16bdc90a398e89b22e8f
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-06-19 00:10:48 -0700

    completed basic styling of add funds frontend component

--- react-app/src/components/AddFunds/index.js
+++ react-app/src/components/AddFunds/index.js
@@ -8,11 +8,21 @@ import { useModal } from "../../context/Modal";
 function AddFunds(){
     // Create state variables
     const [ amount, setAmount ] = useState(0)
+    const [ errors, setErrors ] = useState({})
+
+    // Check input validation on change
+    useEffect(() => {
+        const newErrors = {}
+
+        if(amount < 0) newErrors.amountErr = 'Amount cannot be negative'
+        setErrors(newErrors)
+    }, [amount])
 
 
     // Function to handle form submission
-    const handleSubmit = () => {
-        alert('submit!')
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        alert(`the amount is ${amount}`)
     }
 
 
@@ -20,8 +30,17 @@ function AddFunds(){
         <div className="add-funds-container">
             <div className="header-font" style={{ textAlign: 'center', fontSize: '44px' }}>Add Funds</div>
             <form className="form-container" onSubmit={handleSubmit}>
-                <label className="label-font">Amount</label>
-                <input></input>
+
+                <label className="label-font">Amount $</label>
+                <br/>
+                {errors.amountErr && ( 
+                    <div className="label-font spacecraft-errors">{errors.amountErr}</div>
+                 )}
+                <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} required />
+                <div onClick={handleSubmit} className="button animate">
+                    <div className="hover-effect"></div>
+                    <span className="signup-button-font">Deposit Funds</span>
+                </div>
             </form>
         </div>
     )
