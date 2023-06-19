b8109b667d9f2096c37c4b275bd5b2492a7b0e94
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-06-19 01:54:04 -0700

    completed action creators for transactions slice of state

--- react-app/src/store/transactions.js
+++ react-app/src/store/transactions.js
@@ -3,3 +3,24 @@ const LOAD_TRANSACTIONS = "transactions/loadTransactions"
 const LOAD_TRANSACTION = "transactions/loadTransaction"
 const CREATE_TRANSACTION = "transactions/createTransaction"
 
+// Action Creators
+export const actionLoadTransactions = (transactions) => {
+    return {
+        type: LOAD_TRANSACTIONS,
+        transactions
+    }
+}
+
+export const actionLoadTransaction = (transaction) => {
+    return {
+        type: LOAD_TRANSACTION,
+        transaction
+    }
+}
+
+export const actionCreateTransaction = (transaction) => {
+    return {
+        type: LOAD_TRANSACTION,
+        transaction
+    }
+}
